a7f2bd905c87714dcd1e429dfe20160c94406b0a
Rjibaraoko <junior4553@hotmail.fr>
2023-10-09 18:39:53 +0200

    Huge changes to website
    -Made a js script that makes a transition when we scroll down to the about section of my website
    -added and styled my profile picture so that it scales with the website without beeing too big
    -changed the default position of the website so that it is always zoomed to a certain percentage
    -and decided to make the website all black because I liked the vibe it gave to my project. Clean and minimalistic feel
    -styled every section of my resume to be a seperated block that I will modify later with my real information
    -

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+window.addEventListener('scroll', function() {
+    const aboutMeSection = document.querySelector('.about-me');
+    const sectionTop = aboutMeSection.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    const fadeInThreshold = windowHeight * 0.8; // 80% of the viewport height for fading in
+    const fadeOutThreshold = windowHeight * 0.6; // 60% of the viewport height for fading out
+    
+    if (sectionTop < fadeInThreshold) {
+        aboutMeSection.classList.add('show');
+    } else if (sectionTop > fadeOutThreshold) {
+        aboutMeSection.classList.remove('show');
+    }
+});
+
 document.getElementById('mobile-menu').addEventListener('click', function() {
     var nav = document.querySelector('.nav ul');
     nav.classList.toggle('show');
-});
-
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
 });
